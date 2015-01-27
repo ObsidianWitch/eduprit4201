d5ca5ab0825d874efe00cf156c4ed32a57317974
Obside <obsiagae@gmail.com>
2015-01-27 06:13:44 +0100

    restored compute_dmap* n_iterator_type & p_iterator_type

--- src/tools/compute_dmap.hh
+++ src/tools/compute_dmap.hh
@@ -53,6 +53,8 @@ namespace my {
     typename I::template with_value_type<unsigned>::ret
     compute_dmap__GENERIC(const Image<I>& input_) {
         typedef typename I::point_type point_type;
+        typedef typename I::p_iterator_type p_iterator_type;
+        typedef typename I::n_iterator_type n_iterator_type;
         typedef typename I::template with_value_type<unsigned>::ret dmap_type;
 
         const I& input = input_.exact();
@@ -61,13 +63,13 @@ namespace my {
         const unsigned max = -1;
         dmap_type dmap(D);
 
-        auto p = dmap.pIterator();
+        p_iterator_type p(D);
         for_all(p) {
             dmap(p) = max;
         }
 
         std::queue<point_type> q;
-        auto n = dmap.nIterator();
+        n_iterator_type n;
 
         for_all(p) {
             if (input(p) == true) {
--- src/tools/compute_dmap__v2.hh
+++ src/tools/compute_dmap__v2.hh
@@ -10,6 +10,8 @@ namespace my {
     typename I::template with_value_type<unsigned>::ret
     compute_dmap__v2(const Image<I>& input_, F& f) {
         typedef typename I::point_type point_type;
+        typedef typename I::p_iterator_type p_iterator_type;
+        typedef typename I::n_iterator_type n_iterator_type;
         typedef typename I::template with_value_type<unsigned>::ret dmap_type;
 
         const I& input = input_.exact();
@@ -18,14 +20,13 @@ namespace my {
         const unsigned max = -1;
         dmap_type dmap(D);
 
-        auto p = dmap.pIterator();
+        p_iterator_type p(D);
         for_all(p) {
             dmap(p) = max;
         }
 
         std::queue<point_type> q;
-        auto n = dmap.nIterator();
-
+        n_iterator_type n;
         for_all(p){
             if (input(p) != 0) {
                 // FIXME: cut
