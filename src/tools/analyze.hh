9b169abc87eba9549bb841a270be705c8b8157a9
Obside <obsiagae@gmail.com>
2015-01-30 18:23:46 +0100

    analyse::computeMaxPoints()

--- src/tools/analyze.hh
+++ src/tools/analyze.hh
@@ -1,6 +1,9 @@
 #ifndef ANALYZE_HH
 #define ANALYZE_HH
 
+#include <map>
+#include <iostream>
+
 #include "tools/Image2D.hh"
 #include "tools/ImageIf.hh"
 #include "tools/Helper.hh"
@@ -8,17 +11,51 @@
 
 namespace my {
 
+    std::map<unsigned, Point2D> computeMaxPoints(const Image2D<unsigned>& iz,
+        const Image2D<unsigned>& dmap)
+    {
+        std::map<unsigned, Point2D> m;
+
+        auto p = iz.pIterator();
+        for_all(p) {
+            unsigned label = iz(p);
+            if (label != 0) {
+                if (m.count(label) == 0) {
+                    std::pair<unsigned, Point2D> pair(label, p);
+                    m.insert(pair);
+                }
+                else if (dmap(m[label]) < dmap(p)) {
+                    m[label] = p;
+                }
+            }
+        }
+
+        return m;
+    }
+
     Image2D<unsigned> analyze(Image2D<unsigned>& lab,
         const Image2D<bool_t>& msk)
     {
         Box2D b = lab.domain();
 
-        // FIXME: cut
+        Helper f(lab);
+        auto dmap = compute_dmap__v2(
+            make_ImageIf(lab, msk), f
+        ).removeIf();
+
+        std::map<unsigned, Point2D> maxPoints = computeMaxPoints(f.iz, dmap);
 
         Image2D<unsigned> path(b);
         data_fill(path, 0);
 
-        // FIXME: cut
+        for (auto it = maxPoints.begin() ; it != maxPoints.end() ; it++) {
+            std::cout << "label " << it->first
+                << ":  max distance = " << dmap(it->second)
+                << ",  max point = " << it->second << std::endl << std::endl;
+
+            // TODO PCC
+        }
+
 
         return path;
     }
